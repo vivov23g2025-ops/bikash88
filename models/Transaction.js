d8338566a6a5bcf341c04492e38ea40fdf988615
vivov23g2025-ops <vivov23g2025@gmail.com>
2026-06-13 22:47:32 +0600

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
-const transactionSchema = new mongoose.Schema({
-    sender: { type: String, required: true },
-    receiver: { type: String, required: true },
+const TransactionSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    username: { type: String, required: true },
+    type: { type: String, enum: ['deposit', 'withdrawal'], required: true },
     amount: { type: Number, required: true },
-    type: { type: String, required: true }, // Send Money, Cash In, etc.
-    date: { type: Date, default: Date.now }
+    method: { type: String, enum: ['bkash', 'nagad', 'rocket'], required: true },
+    transactionId: { type: String, default: null }, // ডিপোজিটের TxnID (উইথড্রর জন্য ফাঁকা থাকবে)
+    userNumber: { type: String, required: true }, // যে নম্বর থেকে ক্যাশআউট বা উইথড্র করা হয়েছে
+    status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' }, // এডমিন কন্ট্রোল
+    created_at: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Transaction', transactionSchema);
+module.exports = mongoose.model('Transaction', TransactionSchema);
